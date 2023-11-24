ad0cdfee00d7a20f7e36007ec63dcec5f23223fa
adrianFernandez2003 <sun-moon1814@hotmail.com>
2023-11-24 01:07:40 -0600

    evidencia

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import gui.*;
+
+public class Main {
+    public static void main(String[] args) {
+        RegistroPaciente v = new RegistroPaciente();
+
+    }
+}
---
+++ src/clases/Paciente.java
@@ -0,0 +1,8 @@
+package clases;
+
+public class Paciente {
+    int id;
+    String nombre, apellidoP, apellidoM, numeroTel, correo, genero;
+    String dd,mm,aaaa;
+
+}
